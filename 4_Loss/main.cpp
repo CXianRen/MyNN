a557ea9cb3f5ce2398a606f97a4f2662a53ed364
chenhongguang <chenhongguang@m.scnu.edu.cn>
2023-10-12 18:17:00 +0800

    update 4 and 5 step

--- 4_Loss/main.cpp
+++ 4_Loss/main.cpp
@@ -10,9 +10,16 @@
 
 int main(int argc, char **argv)
 {
-  std::vector<float> input_node(BATCH_SIZE * INPUT_NODE);
-  std::vector<float> y_lable(BATCH_SIZE * OUTPUT_NODE);
+  // init data set
+  int ret = mnist_init("../data/train.txt");
+  if (ret != 0)
+  {
+    std::cout << "Init test fail: should return 0, but " << ret << std::endl;
+    return ret;
+  }
 
+  std::vector<float> input_node, y_lable; // (BATCH_SIZE * INPUT_NODE); will be filled by mnist_get_random()
+  mnist_random_get(input_node, y_lable, BATCH_SIZE);
   std::vector<float> l1_node(INPUT_NODE * L1_NODE);
   random_init_layer(l1_node);
   std::vector<float> l2_node(L1_NODE * L2_NODE);
@@ -25,6 +32,8 @@ int main(int argc, char **argv)
   auto a2 = relu(dot(a1, l2_node, BATCH_SIZE, L1_NODE, L2_NODE));
   auto yp = softmax(dot(a2, output_node, BATCH_SIZE, L2_NODE, OUTPUT_NODE), BATCH_SIZE, OUTPUT_NODE);
   print_m(yp, BATCH_SIZE, OUTPUT_NODE);
+  std::cout << "bench mark:" << std::endl;
+  print_m(y_lable, BATCH_SIZE, OUTPUT_NODE);
 
   auto loss = Loss_MSE(yp, y_lable);
   std::cout << "Loss is:" << loss << std::endl;
