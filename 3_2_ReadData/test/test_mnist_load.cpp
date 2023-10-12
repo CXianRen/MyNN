f03dd6251ca34f3e0ee5c69f43fa9b66fd2b7227
chenhongguang <chenhongguang@m.scnu.edu.cn>
2023-10-12 17:23:16 +0800

    update test case for mnist_init()

--- 3_2_ReadData/test/test_mnist_load.cpp
+++ 3_2_ReadData/test/test_mnist_load.cpp
@@ -14,20 +14,30 @@ void test_init()
   }
 
   {
-    int ret = mnist_init("../data/train.txt");
+    int ret = mnist_init("../data/test_data.txt");
     if (ret != 0)
     {
       std::cout << "Init test fail: should return 0, but " << ret << std::endl;
     }
     auto imgs = mnist_get_imgs();
-    if (imgs[0].size() != 784){
-      std::cout << "img 0 size is not 784, but " << imgs[0].size()  << std::endl;
+    if (imgs[0].size() != 784)
+    {
+      std::cout << "img 0 size is not 784, but " << imgs[0].size() << std::endl;
     }
     auto labels = mnist_get_labels();
-    print_m(labels[0],1,10);
-    if (labels[0][1] != 1.0){
-      std::cout << "labels[0][1] is not 1, but " << labels[0][1]  << std::endl;
-    } 
+    print_m(labels[0], 1, 10);
+    if (labels[0][0] != 1.0)
+    {
+      std::cout << "labels[0][1] is not 1, but " << labels[0][1] << std::endl;
+    }
+    for (int i = 0; i < 784; i++)
+    {
+      if (imgs[0][i] != i+1)
+      {
+        std::cout << "test imgs[0][" << i << "] is not " << i+1 << ", but " << imgs[0][i] << std::endl;
+        break;
+      }
+    }
   }
 }
 
