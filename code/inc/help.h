8221056d3cb2f55adf91541ef2a23061fe654722
chenhongguang <chenhongguang@m.scnu.edu.cn>
2024-06-09 16:46:51 +0200

    add random initializer

--- code/inc/help.h
+++ code/inc/help.h
@@ -4,6 +4,19 @@
 namespace Potato::helper
 {
 
+  /*
+   * random initialer
+   * [-1,1]
+   */
+  template <typename T>
+  void random_init(T &a, const int size)
+  {
+    for (int i = 0; i < size; i++)
+    {
+      a[i] = (rand() % 2000 - 1000) / 1000.0;
+    }
+  }
+
 } // namespace Potato::helper
 
 #endif // __POTATO_HELP_H__
